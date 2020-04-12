46d1b0a7ada00b9e49794769a67660167a5bdaca
avarashen <avarashen@exadel.com>
2020-04-12 13:49:16 +0300

    setup base structure

---
+++ src/main/java/org/feeder/api/application/FeederApplication.java
@@ -0,0 +1,12 @@
+package org.feeder.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeederApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeederApplication.class, args);
+  }
+}
